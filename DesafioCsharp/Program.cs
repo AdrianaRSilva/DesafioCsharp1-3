5f2baa121321d61b684623c9faf1929d24cff523
AdrianaRSilva <70419354+AdrianaRSilva@users.noreply.github.com>
2021-01-19 21:46:16 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioCsharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Desafio
+{
+    static void Main()
+    {
+        var km = Int32.Parse(Console.ReadLine());
+        int minutos = km * 2; 
+        Console.WriteLine(minutos + " minutos");
+    }
+}
